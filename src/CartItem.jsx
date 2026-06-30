f02907c815306ab586bfe803d0070c15c730e324
Shinobidono <kouchamif@gmail.com>
2026-06-30 07:09:10 -0400

    final project task4

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,55 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total = 0;
+
+    cart.forEach(item => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+
+    return total;
  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+  
 
   const handleIncrement = (item) => {
+    dispatch(
+        updateQuantity({
+            name: item.name,
+            quantity: item.quantity + 1
+        })
+    );
   };
 
   const handleDecrement = (item) => {
-   
+    /* If the item's quantity is greater than 1, dispatch updateQuantity to decrease the quantity by 1. Else if the quantity would drop to 0, dispatch the removeItem action to remove the plant type from the cart */
+    if (item.quantity > 1) {
+        dispatch(
+            updateQuantity({
+                name: item.name,
+                quantity: item.quantity - 1
+            })
+        );
+    }
+    else {
+        dispatch(
+            removeItem(item.name)
+        );
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(
+        removeItem(item.name)
+    )
   };
 
   // Calculate total cost based on quantity for an item
